a1700541c3cb26f9ac842b98bd8272ca761fc053
murpl239 <lara.murphy239@mail.dcu.ie>
2022-02-24 19:55:11 +0000

    Initialised fronend

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <!-- <v-app-bar color="green"><h1>WHATSUPO</h1></v-app-bar> -->
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ frontend/src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import TestView from '../views/TestView.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/test',
+    name: 'test',
+    component: TestView
+  },
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
